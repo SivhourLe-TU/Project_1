b0c20527f03bb1b90fac3e9451997d9e8ea129fd
SivhourLe-TU <sivhour.le@temple.edu>
2026-09-08 21:27:08 -0400

    open and read

--- tucat.c
+++ tucat.c
@@ -2,14 +2,31 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int main() {
+int main(int argc, char *argv[]) {
     
-	int fd = open("example.txt", O_RDONLY);
-	if (fd == -1) {
-		perror("tucat: open");
+	char buff[4096];
+
+	for(int i =1; i < argc; i++){
+		int fd = open(argv[i], O_RDONLY);
+
+		if  (fd == -1){
+			perror("tucat: open");
+			return 1;
+		}
+
+	
+
+	ssize_t n;
+	while ((n = read(fd, buff, sizeof(buff))) > 0) {
+		write(STDOUT_FILENO, buff, n);
+	}
+	
+
+	if (n == -1){
+		perror("tucat: read");
 		return 1;
 	}
-	printf("fd = %d\n", fd);
+
 	close(fd);
 
 	return 0;
