177006c9c8b89bdb1c7c6ee8d60c4d2016936a21
SivhourLe-TU <sivhour.le@temple.edu>
2026-09-08 21:31:26 -0400

    tucat changes

--- tucat.c
+++ tucat.c
@@ -3,31 +3,27 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    
-	char buff[4096];
+    char buf[4096];
 
-	for(int i =1; i < argc; i++){
-		int fd = open(argv[i], O_RDONLY);
+    for (int i = 1; i < argc; i++) {
+        int fd = open(argv[i], O_RDONLY);
 
-		if  (fd == -1){
-			perror("tucat: open");
-			return 1;
-		}
+        if (fd == -1) {
+            perror("tucat: open");
+            return 1;
+        }
 
-	
+        ssize_t n;
+        while ((n = read(fd, buf, sizeof(buf))) > 0) {
+            write(STDOUT_FILENO, buf, n);
 
-	ssize_t n;
-	while ((n = read(fd, buff, sizeof(buff))) > 0) {
-		write(STDOUT_FILENO, buff, n);
-	}
-	
+        if (n == -1) {
+            perror("tucat: read");
+            return 1;
+        }
 
-	if (n == -1){
-		perror("tucat: read");
-		return 1;
-	}
+        close(fd);
+    }
 
-	close(fd);
-
-	return 0;
+    return 0;
 }
